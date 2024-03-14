115fbb529d1c449106bdf928352fa378251e5318
DragonJT <jonathanthoms006@gmail.com>
2024-03-14 17:17:27 +0000

    refactored tokenizer to give token type

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿
 
-
 enum JOpcode{I32Const, F32Const, GetLocal, BinaryOp, UnaryOp, SetLocal, CreateLocal, Call, GotoIf, Label, Goto}
 class Instruction(JOpcode opcode, string value){
     public JOpcode opcode = opcode;
@@ -143,78 +142,124 @@ class VM{
     }
 }
 
-class Tokenizer(string code)
-{
-    string code = code;
-    int index = 0;
-    bool split = true;
-    int start = 0;
-    int depth = 0;
-    public List<string> tokens = [];
+enum TokenType{Varname, Int, Float, Curly, Square, Parens, Punctuation}
 
-    void AddPrevToken(int delta = 0){
-        if(depth==0){
-            if(!split){
-                tokens.Add(code[start..(index+delta)]);
-            }
-            split = true;
+class Token(string value, int start, int end, TokenType type){
+    public string value = value;
+    public int start = start;
+    public int end = end;
+    public TokenType type = type;
+}
+
+static class Tokenizer{
+    public static List<Token> Tokenize(string code){
+        var index = 0;
+        var tokens = new List<Token>();
+        var open = "({[";
+        var close = ")}]";
+
+        loop:
+        if(index>=code.Length){
+            return tokens;
         }
-    }
-
-    public void Tokenize(){
-        var punctuation = ";+-*/=<>";
-        var open = "([{";
-        var close = ")]}";
-
-        for(index=0;index<code.Length;index++){
-            var c = code[index];
-            if(close.Contains(c)){
-                depth--;
-                if(depth<0){
-                    throw new Exception("Depth is < 0");
+        var c = code[index];
+        if(char.IsWhiteSpace(c)){
+            index++;
+            goto loop;
+        }
+        if(char.IsLetter(c)){
+            var start = index;
+            index++;
+            while(true){
+                if(index>=code.Length){
+                    tokens.Add(new Token(code[start..index], start, index, TokenType.Varname));
+                    return tokens;
                 }
-                AddPrevToken(1);
-            }
-            else if(char.IsWhiteSpace(c)){
-                AddPrevToken();
-            }
-            else if(punctuation.Contains(c)){
-                AddPrevToken();
-                if(depth==0){
-                    tokens.Add(code[index..(index+1)]);
+                c = code[index];
+                if(char.IsLetter(c) || char.IsDigit(c)){
+                    index++;
+                    continue;
                 }
-            }
-            else if(open.Contains(c)){
-                AddPrevToken();
-                if(depth==0){
-                    split = false;
-                    start = index;
-                }
-                depth++;
-            }
-            else{
-                if(split){
-                    split = false;
-                    start = index;
-                }
+                tokens.Add(new Token(code[start..index], start, index, TokenType.Varname));
+                goto loop;
             }
         }
-        AddPrevToken();
-    }
-
-    public static List<string> Tokenize(string code){
-        var tokenizer = new Tokenizer(code);
-        tokenizer.Tokenize();
-        return tokenizer.tokens;
+        else if(char.IsDigit(c)){
+            var start = index;
+            index++;
+            var type = TokenType.Int;
+            while(true){
+                if(index>=code.Length){
+                    tokens.Add(new Token(code[start..index], start, index, type));
+                    return tokens;
+                }
+                c = code[index];
+                if(c=='.'){
+                    type = TokenType.Float;
+                    index++;
+                    continue;
+                }
+                else if(char.IsDigit(c)){
+                    index++;
+                    continue;
+                }
+                tokens.Add(new Token(code[start..index], start, index, type));
+                goto loop;
+            }
+        }
+        else if(open.Contains(c)){
+            var start = index;
+            var depth = 1;
+            index++;
+            while(true){
+                if(index>=code.Length){
+                    throw new Exception("No close braces before end of file");
+                }
+                c = code[index];
+                if(open.Contains(c)){
+                    index++;
+                    depth++;
+                    continue;
+                }
+                else if(close.Contains(c)){
+                    index++;
+                    depth--;
+                    if(depth<1){
+                        if(code[start] == '{' && code[index-1]=='}'){
+                            tokens.Add(new Token(code[(start+1)..(index-1)], start, index, TokenType.Curly));
+                            goto loop;
+                        }
+                        else if(code[start] == '[' && code[index-1]==']'){
+                            tokens.Add(new Token(code[(start+1)..(index-1)], start, index, TokenType.Square));
+                            goto loop;
+                        }
+                        else if(code[start] == '(' && code[index-1]==')'){
+                            tokens.Add(new Token(code[(start+1)..(index-1)], start, index, TokenType.Parens));
+                            goto loop;
+                        }
+                        else{
+                            throw new Exception("Unexpected start and end of braces: "+code[start]+code[index-1]);
+                        }
+                    }
+                    continue;
+                }
+                index++;
+            }
+        }
+        else{
+            tokens.Add(new Token(code[index].ToString(), index, index+1, TokenType.Punctuation));
+            index++;
+            goto loop;
+        }
     }
 }
 
 static class Parser{
-    static List<List<string>> SplitByComma(List<string> tokens){
-        List<List<string>> output = [];
+    static List<List<Token>> SplitByComma(List<Token> tokens){
+        List<List<Token>> output = [];
         var start = 0;
         for(var i=0;i<tokens.Count;i++){
-            if(tokens[i] == ","){
+            if(tokens[i].value == ","){
                 output.Add(tokens[start..i]);
                 start = i+1;
             }
@@ -225,88 +270,88 @@ static class Parser{
         return output;
     }
 
-    static Instruction[] ParseExpressionInParens(string code){
-        return ParseExpression(Tokenizer.Tokenize(code[1..^1]));
+    static Instruction[] ParseExpressionInParens(Token token){
+        return ParseExpression(Tokenizer.Tokenize(token.value));
     }
 
-    public static Instruction[] ParseExpression(List<string> tokens){
+    public static Instruction[] ParseExpression(List<Token> tokens){
         var operators = new string[][]{["<", ">"], ["+", "-"], ["/", "*"]};
         if(tokens.Count == 0){
             throw new Exception("No tokens");
         }
         else if(tokens.Count == 1){
-            if(tokens[0][0] == '('){
+            if(tokens[0].type == TokenType.Parens){
                 return ParseExpressionInParens(tokens[0]);
             }
-            else if(char.IsDigit(tokens[0][0])){
-                if(tokens[0].Contains('.')){
-                    return [new Instruction(JOpcode.F32Const, tokens[0])];
-                }
-                return [new Instruction(JOpcode.I32Const, tokens[0])];
+            else if(tokens[0].type == TokenType.Float){
+                return [new Instruction(JOpcode.F32Const, tokens[0].value)];
             }
-            else if(char.IsLetter(tokens[0][0])){
-                return [new Instruction(JOpcode.GetLocal, tokens[0])];
+            else if(tokens[0].type == TokenType.Int){
+                return [new Instruction(JOpcode.I32Const, tokens[0].value)];
+            }
+            else if(tokens[0].type == TokenType.Varname){
+                return [new Instruction(JOpcode.GetLocal, tokens[0].value)];
             }
             else{
                 throw new Exception("Unexpected token: "+tokens[0]);
             }
         }
         else if(tokens.Count == 2){
-            if(char.IsLetter(tokens[0][0]) && tokens[1][0] == '('){
-                var args = SplitByComma(Tokenizer.Tokenize(tokens[1][1..^1])).Select(ParseExpression).ToArray();
-                return [.. args.SelectMany(a=>a).ToArray(), new Instruction(JOpcode.Call, tokens[0])];
+            if(tokens[0].type == TokenType.Varname && tokens[1].type == TokenType.Parens){
+                var args = SplitByComma(Tokenizer.Tokenize(tokens[1].value)).Select(ParseExpression).ToArray();
+                return [.. args.SelectMany(a=>a).ToArray(), new Instruction(JOpcode.Call, tokens[0].value)];
             }
         }
         foreach(var ops in operators){
-            var index = tokens.FindLastIndex(t=>ops.Contains(t));
+            var index = tokens.FindLastIndex(t=>ops.Contains(t.value));
             if(index>=0){
                 var left = ParseExpression(tokens[0..index]);
                 var right = ParseExpression(tokens[(index+1)..tokens.Count]);
-                return [..left, ..right, new Instruction(JOpcode.BinaryOp, tokens[index])];
+                return [..left, ..right, new Instruction(JOpcode.BinaryOp, tokens[index].value)];
             }
         }
         throw new Exception("Unexpected tokens");
     }
 
     static Instruction[] ParseBody(string code, List<string> blocks, ref int labelID){
-        var tokens = Tokenizer.Tokenize(code[1..^1]);
+        var tokens = Tokenizer.Tokenize(code);
         List<Instruction> instructions = [];
         var i = 0;
         while(true){
             if(i>=tokens.Count){
                 return [..instructions];
             }
-            if(i+1<tokens.Count && tokens[i+1] == "="){
-                var end = tokens.IndexOf(";", i+2);
+            if(i+1<tokens.Count && tokens[i+1].value == "="){
+                var end = tokens.FindIndex(i+2, t=>t.value == ";");
                 if(end<0){
                     throw new Exception("No end of expression");
                 }
                 instructions.AddRange(ParseExpression(tokens[(i+2)..end]));
-                instructions.Add(new Instruction(JOpcode.SetLocal, tokens[i]));
+                instructions.Add(new Instruction(JOpcode.SetLocal, tokens[i].value));
                 i = end+1;
             }
-            else if(tokens[i] == "return"){
-                var end = tokens.IndexOf(";", i+1);
+            else if(tokens[i].value == "return"){
+                var end = tokens.FindIndex(i+1, t=>t.value == ";");
                 if(end<0){
                     throw new Exception("No end of expression");
                 }
                 instructions.AddRange(ParseExpression(tokens[(i+1)..end]));
                 i = end+1;
             }
-            else if(tokens[i] == "if"){
+            else if(tokens[i].value == "if"){
                 instructions.AddRange(ParseExpressionInParens(tokens[i+1]));
                 instructions.Add(new Instruction(JOpcode.UnaryOp, "!"));
                 instructions.Add(new Instruction(JOpcode.GotoIf, labelID.ToString()));
-                instructions.AddRange(ParseBody(tokens[i+2], blocks, ref labelID));
+                instructions.AddRange(ParseBody(tokens[i+2].value, blocks, ref labelID));
                 instructions.Add(new Instruction(JOpcode.Label, labelID.ToString()));
                 labelID++;
                 i+=3;
             }
-            else if(tokens[i] == "break"){
+            else if(tokens[i].value == "break"){
                 instructions.Add(new Instruction(JOpcode.Goto, blocks[^1]));
                 i+=2;
             }
-            else if(tokens[i] == "while"){
+            else if(tokens[i] .value== "while"){
                 var startLabelID = labelID.ToString();
                 var endLabelID = (labelID+1).ToString();
                 labelID+=2;
@@ -315,15 +360,15 @@ static class Parser{
                 instructions.Add(new Instruction(JOpcode.UnaryOp, "!"));
                 instructions.Add(new Instruction(JOpcode.GotoIf, endLabelID));
                 blocks.Add(endLabelID);
-                instructions.AddRange(ParseBody(tokens[i+2], blocks, ref labelID));
+                instructions.AddRange(ParseBody(tokens[i+2].value, blocks, ref labelID));
                 blocks.RemoveAt(blocks.Count-1);
                 instructions.Add(new Instruction(JOpcode.Goto, startLabelID));
                 instructions.Add(new Instruction(JOpcode.Label, endLabelID));
                 i+=3;
             }
-            else if(tokens[i] == "var"){
-                var name = tokens[i+1];
-                var end = tokens.IndexOf(";", i+3);
+            else if(tokens[i].value == "var"){
+                var name = tokens[i+1].value;
+                var end = tokens.FindIndex(i+3, t=>t.value == ";");
                 if(end<0){
                     throw new Exception("No end of expression");
                 }
@@ -333,7 +378,7 @@ static class Parser{
                 i = end+1;
             }
             else{
-                var end = tokens.IndexOf(";", i);
+                var end = tokens.FindIndex(i, t=>t.value == ";");
                 if(end<0){
                     throw new Exception("No end of expression");
                 }
@@ -350,8 +395,8 @@ static class Parser{
     }
 
     static Parameter[] ParseParameters(string code){
-        var parameterTokens = SplitByComma(Tokenizer.Tokenize(code[1..^1]));
-        return parameterTokens.Select(p=>new Parameter(p[0], p[1])).ToArray();
+        var parameterTokens = SplitByComma(Tokenizer.Tokenize(code));
+        return parameterTokens.Select(p=>new Parameter(p[0].value, p[1].value)).ToArray();
     }
 
     public static Function[] ParseFunctions(string code){
@@ -364,9 +409,9 @@ static class Parser{
             }
             var returnType = tokens[i];
             var name = tokens[i+1];
-            var parameters = ParseParameters(tokens[i+2]);
-            var body = ParseBody(tokens[i+3]);
-            functions.Add(new Function(returnType, name, parameters, body));
+            var parameters = ParseParameters(tokens[i+2].value);
+            var body = ParseBody(tokens[i+3].value);
+            functions.Add(new Function(returnType.value, name.value, parameters, body));
             i+=4;
         }
     }
